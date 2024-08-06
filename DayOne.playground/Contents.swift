9680aaf5330c46a03d75b110a0428f4c78720d3d
ElGatoDaniel <daniellizamaperaza@gmail.com>
2024-08-05 22:46:21 -0500

    Add files via upload

---
+++ DayOne.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "Danny"
+
+let birthplace = "México"
+var age: Int = 28
+let pi: Double = 3.14
+var answer: Bool = true
+
+let sentence = "My name is \(str), i'm \(age) years old and i was born in \(birthplace)"
+
+let multiline = """
+        I'm a multiline string text
+        Isn't it awesome?
+        I think it is.
+"""
+
+
